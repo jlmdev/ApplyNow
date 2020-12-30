71244e3693c12bcd8835f67c391cfdf32e685fc9
Josh Mann <jlmann72@gmail.com>
2020-12-30 09:42:26 -0500

    task div created

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -7,12 +7,23 @@ export function App() {
       <div className="header-container">
         <header>
           <ul>
-            <li>icon</li>
+            <li><img src="https://img.icons8.com/metro/26/000000/menu.png"/></li>
             <li>ToDo</li>
           </ul>
         </header>
         <div className="header-spacer"></div>
       </div>
+      <div className="task">
+        <ul>
+          <li>Description:</li>
+          <li>Priority:</li>
+          <li>Start Date:</li>
+          <li>Due Date:</li>
+          <li>Notes:</li>
+        </ul>
+      </div>
+
+      <a href="https://icons8.com/icon/3095/menu">Menu icon by Icons8</a>
     </>
   )
 }
